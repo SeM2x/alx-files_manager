88639b64872f9218cadeef8d3fc745f2b17c747a
SeM2x <houssem135.302@gmail.com>
2024-12-12 10:48:14 +0100

    0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,26 @@
+import client from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = client.createClient().on_error((err) => console.log(err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.client.get(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
